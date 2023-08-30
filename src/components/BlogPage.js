59466ccf11f867d2d21304da00549d1e79301a8e
nakairyo19 <ryoryoest19@gmail.com>
2023-08-30 10:18:32 +0900

    Create src

---
+++ src/components/BlogPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BlogPage = () => {
+  return (
+    <div className='container'>
+      <h1>ブログ</h1>
+      <p>サンプル</p>
+    </div>
+  )
+}
+
+export default BlogPage
